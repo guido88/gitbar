7ac8d59e482e21219214e3c6c589bf98746992ce
guido88 <guido211@hotmail.it>
2019-02-01 22:03:48 +0000

    Added DOM manipulation and mouse event handling to display user profile info

--- git-app.js
+++ git-app.js
@@ -1,6 +1,7 @@
 const searchButton = document.querySelector(".git-button");
 const inputValue = document.querySelector("#git-user");
-
+const resultList = document.querySelector(".git-search-list");
+const information = document.querySelector(".information");
 
 const client_secret = "c3aaf3585174b2f2f9b1c82dae1e0dae539c1d25";
 const client_id = "Iv1.e39651f823cce1c7";
@@ -24,11 +25,59 @@ const showData = () => {
 };
 
 
-searchButton.addEventListener("click", () => {
+inputValue.addEventListener("input", () => {
+		
+	if(inputValue.value.length>1){
+		getUsers(inputValue.value).then((res) =>{
+			console.log(res);
+			resultList.innerHTML="";
+			for(const user of res.data.items){
 
-		showData();
+				const div = createDivUser(user);
+
+				div.addEventListener("click", () =>{
+					information.innerHTML="";
+					information.appendChild(createImg(user.avatar_url));
+					information.appendChild(createInfoUser(user.login));
+					information.appendChild(createInfoUser(user.html_url));
+				});
+
+				resultList.appendChild(div);
+			}
+
+	}); }
 
 });
 
 
-// 52cfce67982765ef6235afb88eca9463f6903068
+const createDivUser = (user) => {
+	
+	let div = document.createElement("div");
+	div.classList.add("div-user");
+	div.innerHTML = user.login;
+
+	return div;
+};
+
+const createInfoUser = (info) => {
+	
+	let span = document.createElement("span");
+	span.innerHTML = info;
+
+	return span;
+};
+
+const createImg = (url_img) => {
+	
+	let img = document.createElement("img");
+	img.setAttribute("src",url_img);
+
+	return img;
+};
+
+
+document.addEventListener("click", (e) =>{
+		
+		if(e.target!=inputValue)
+		resultList.innerHTML="";
+});
